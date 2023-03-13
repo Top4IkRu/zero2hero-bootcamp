53f3dd07e373f78c3aee2ceb8b61e5f7cf99968c
Leonid Zemenkov <zmaster-leo@yandex.ru>
2023-03-13 20:36:09 +0300

    add capture + ddb + serverless

--- app.js
+++ app.js
@@ -1,30 +1,71 @@
-const express = require('express')
+const express = require('express');
 const {ethers} = require("ethers");
+const AWS = require("aws-sdk")
+const sls = require('serverless-http');
+const axios = require("axios");
 
 const app = express()
 app.use(express.json());
 // disable cors for local testing
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://bnb-faucet-amber.vercel.app");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
-app.get("/test-path/4312", (req, res) => {
-    console.log(req);
-    res.send("ok");
+const dynamoDB = new AWS.DynamoDB({
+    region: "us-east-1",
 })
 
+const privateKey = "0x0a9f83caeafc1350bf93c3bf0c93fff20c4562c075b4628b57c9b42e7b4e3e96";
+const from = "0xc0dec91957A1839E899f0318440192D7E618c26C";
+const amountValue = "0.05";
 
-const port = 8080
+const googleCaptureSecret = process.env.CAPTURE_SECRET
 
-const privateKey = "0x3126ba32d11c7f669ca2cfcee3b9caad9de600e4e3b6abb1655b07b8179cea9f";
-const from = "0xc0dEdbFD9224c8C7e0254825820CC706180259F2";
-const amountValue = "0.0001";
+async function isValidToken(token) {
+    try {
+        let response = await axios.post(`https://www.google.com/recaptcha/api/siteverify?secret=${googleCaptureSecret}&response=${token}`);
+        console.log('Google response = ' + JSON.stringify(response.data));
+        return response.data.success;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
 
 app.post('/send', async (req, res) => {
     const address = req.body.address;
-    console.log("Sending tBNB to " + address)
+    const token = req.body.captureToken;
+    console.log("Sending tBNB to " + address);
+    console.log("Token " + token);
+
+    const items = await dynamoDB.scan({TableName: "BnbFaucet"}).promise().then(data => data.Items)
+    const lastTx = items.find(item => item.address.S === address)
+
+    const verificationStatus = await isValidToken(token);
+    if (!verificationStatus) {
+        res.send({
+            status: "error",
+            errorMessage: 'Capture token is invalid. Please try again.'
+        });
+        return;
+    }
+
+    if (lastTx !== undefined) {
+        const lastTxDate = new Date(Number(lastTx.instant.N));
+        const lastTxIsOk = new Date().getTime() - lastTxDate > 43200000;
+        if (!lastTxIsOk) {
+            const nextPayoutDate = new Date(lastTxDate.getTime() + 43200000);
+            res.send({
+                status: "error",
+                errorMessage: 'You have recently received coins. Next payout in ' +
+                    nextPayoutDate.toLocaleDateString() + ' ' + nextPayoutDate.toLocaleTimeString()
+            });
+            return;
+        }
+    }
+
 
     const provider = ethers.getDefaultProvider("https://data-seed-prebsc-1-s1.binance.org:8545");
     const wallet = new ethers.Wallet(privateKey, provider);
@@ -37,6 +78,25 @@ app.post('/send', async (req, res) => {
             value: amount,
             chainId: 97
         });
+
+        await dynamoDB.putItem({
+            TableName: "BnbFaucet",
+            Item: {
+                address: {
+                    S: address
+                },
+                amount: {
+                    S: amountValue
+                },
+                instant: {
+                    N: new Date().getTime().toString()
+                }
+            },
+        })
+            .promise()
+            .then(data => console.log(data))
+            .catch(console.error)
+
         res.send({
             status: "success",
             hash: sentTx.hash,
@@ -59,6 +119,9 @@ app.post('/send', async (req, res) => {
     }
 });
 
+const port = 8080
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+module.exports.server = sls(app)
